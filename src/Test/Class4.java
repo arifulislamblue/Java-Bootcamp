74ba8e919e0345ab344387bde4aeb3baa86bd9a6
Ariful Islam <62497537+arifulislamblue@users.noreply.github.com>
2022-06-27 13:24:27 +0600

    Delete src/Test directory

--- src/Test/Class4.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class Class4 {
-    public static void main(String[] args) {
-        System.out.println("push");
-    }
-}
--- src/Test/test3.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class test3 {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}
